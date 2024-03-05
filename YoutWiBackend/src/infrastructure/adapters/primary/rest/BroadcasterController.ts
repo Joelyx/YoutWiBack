b2c828fd6c45a5e62cc30ce550f39e1de63c56a0
Joel Hernández <joelyx96@gmail.com>
2024-03-05 15:31:52 +0000

    feat: se puede recoger los canales que andan en directo

--- YoutWiBackend/src/infrastructure/adapters/primary/rest/BroadcasterController.ts
+++ YoutWiBackend/src/infrastructure/adapters/primary/rest/BroadcasterController.ts
@@ -47,14 +47,57 @@ class BroadcasterController {
 
     public findUserFollowedBroadcasters = async (req: Request, res: Response) => {
         const userId: string = req.user.userId;
+        const twitchClientId = process.env.TWITCH_CLIENT_ID ?? "";
+
         try {
             const broadcasters = await this.broadcasterDomainService.findUserFollowedBroadcasters(userId);
-            res.status(200).json(broadcasters);
+            const broadcasterIds = broadcasters.map(broadcaster => broadcaster.id);
+
+            const isLive = await this.checkIfBroadcastersAreLive(broadcasterIds, twitchClientId);
+            const liveBroadcasters = broadcasters.filter(broadcaster => isLive[broadcaster.id]);
+
+            res.status(200).json(liveBroadcasters);
         } catch (error) {
             console.error('Error finding broadcasters:', error);
             res.status(500).json({ message: 'Failed to find broadcasters' });
         }
     }
+
+    private async checkIfBroadcastersAreLive(broadcasterIds: string[], clientId: string): Promise<{[key: string]: boolean}> {
+        const twitchAppAccessToken = await this.getTwitchAppAccessToken();
+        const url = `https://api.twitch.tv/helix/streams?${broadcasterIds.map(id => `user_id=${id}`).join('&')}`;
+        const headers = {
+            'Client-ID': clientId,
+            Authorization: `Bearer ${twitchAppAccessToken}`
+        };
+
+        try {
+            const response = await axios.get(url, { headers });
+            const liveBroadcasters = response.data.data.reduce((acc: {[key: string]: boolean}, broadcaster: any) => {
+                acc[broadcaster.user_id] = true;
+                return acc;
+            }, {});
+
+            return liveBroadcasters;
+        } catch (error) {
+            console.error('Error checking if broadcasters are live:', error);
+            return {};
+        }
+    }
+
+    private async getTwitchAppAccessToken() {
+        const clientId = process.env.TWITCH_CLIENT_ID;
+        const clientSecret = process.env.TWITCH_CLIENT_SECRET;
+
+        try {
+            const response = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`);
+            return response.data.access_token;
+        } catch (error) {
+            console.error('Error getting Twitch App Access Token:', error);
+            return null;
+        }
+    };
+
 }
 
 export default BroadcasterController;
