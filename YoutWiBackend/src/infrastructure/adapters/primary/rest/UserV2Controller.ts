a88821825b5580ba7ef7acc0c939487e11cc7e79
Joel Hernández <joelyx96@gmail.com>
2024-05-27 20:24:08 +0100

    fix: redirect

--- YoutWiBackend/src/infrastructure/adapters/primary/rest/UserV2Controller.ts
+++ YoutWiBackend/src/infrastructure/adapters/primary/rest/UserV2Controller.ts
@@ -74,7 +74,7 @@ export default class UserV2Controller {
         } else {
             //const defaultImagePath = path.resolve(__dirname, '..', '..', '..', '..', 'public', 'images', 'default.png');
             //res.sendFile(defaultImagePath);
-            res.sendFile("https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg");
+            res.redirect("https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg");
         }
     };
 
